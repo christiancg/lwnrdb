57b7b64ad277ee459a42d048fdc6baa7475811ea
Christian Coello Grassi <christian.coello.g@gmail.com>
2024-08-31 11:32:40 -0300

    feat: done some TODOs

--- src/main/java/org/techhouse/ejson/type_adapters/impl/EnumTypeAdapter.java
+++ src/main/java/org/techhouse/ejson/type_adapters/impl/EnumTypeAdapter.java
@@ -2,10 +2,13 @@ package org.techhouse.ejson.type_adapters.impl;
 
 import org.techhouse.ejson.elements.JsonBaseElement;
 import org.techhouse.ejson.type_adapters.TypeAdapter;
+import org.techhouse.log.Logger;
 
 import java.lang.reflect.Method;
 
 public class EnumTypeAdapter<T extends Enum<T>> implements TypeAdapter<T> {
+
+    private final Logger logger = Logger.logFor(EnumTypeAdapter.class);
 
     private final Class<T> clazz;
 
@@ -25,8 +28,8 @@ public class EnumTypeAdapter<T extends Enum<T>> implements TypeAdapter<T> {
                 Method valueOf = clazz.getMethod("valueOf", String.class);
                 Object enumValue = valueOf.invoke(null, value.asJsonString().getValue());
                 return clazz.cast(enumValue);
-            } catch (Exception ignored) {
-                // TODO: probably log error here
+            } catch (Exception e) {
+                logger.error("Error while parsing enum value: ", e);
             }
         }
         return null;
