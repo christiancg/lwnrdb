b2b77adcc4187c58496a8a10955a5e7658629fa2
Christian Coello Grassi <christian.coello.g@gmail.com>
2024-09-14 13:59:23 -0300

    feat: adding tests for bckg_ops package

---
+++ src/test/java/org/techhouse/bckg_ops/EventProcessorHelperTest.java
@@ -0,0 +1,113 @@
+package org.techhouse.bckg_ops;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.techhouse.bckg_ops.events.*;
+import org.techhouse.data.DbEntry;
+import org.techhouse.data.admin.AdminCollEntry;
+import org.techhouse.data.admin.AdminDbEntry;
+import org.techhouse.ejson.elements.JsonObject;
+import org.techhouse.ops.AdminOperationHelper;
+import org.techhouse.test.TestGlobals;
+import org.techhouse.test.TestUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class EventProcessorHelperTest {
+
+    @BeforeEach
+    public void setUp() throws NoSuchFieldException, IllegalAccessException, IOException {
+        TestUtils.standardInitialSetup();
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException, InterruptedException {
+        TestUtils.standardTearDown();
+    }
+
+    @Test
+    public void processDatabaseEventDeletionTest() throws IOException, InterruptedException {
+        final var databaseEventCreate = new DatabaseEvent(EventType.CREATED, TestGlobals.DB);
+        EventProcessorHelper.processEvent(databaseEventCreate);
+        final var databaseEventDelete = new DatabaseEvent(EventType.DELETED, TestGlobals.DB);
+        EventProcessorHelper.processEvent(databaseEventDelete);
+        final var dbEntry = AdminOperationHelper.getDatabaseEntry(TestGlobals.DB);
+        Assertions.assertNull(dbEntry);
+    }
+
+    @Test
+    public void processCollectionEventCreationTest() throws IOException, InterruptedException {
+        final var adminEvent = new DatabaseEvent(EventType.CREATED, TestGlobals.DB);
+        EventProcessorHelper.processEvent(adminEvent);
+        final var collectionEvent = new CollectionEvent(EventType.CREATED, TestGlobals.DB, TestGlobals.COLL);
+        EventProcessorHelper.processEvent(collectionEvent);
+        final var collEntry = AdminOperationHelper.getCollectionEntry(TestGlobals.DB, TestGlobals.COLL);
+        Assertions.assertNotNull(collEntry);
+    }
+
+    @Test
+    public void processCollectionEventDeletionTest() throws IOException, InterruptedException {
+        var collectionEvent = mock(CollectionEvent.class);
+        var adminOperationHelper = mock(AdminOperationHelper.class);
+        when(collectionEvent.getDbName()).thenReturn(TestGlobals.DB);
+        when(collectionEvent.getCollName()).thenReturn(TestGlobals.COLL);
+        when(collectionEvent.getType()).thenReturn(EventType.DELETED);
+        EventProcessorHelper.processEvent(collectionEvent);
+        verify(adminOperationHelper, times(1)).deleteCollectionEntry(TestGlobals.DB, TestGlobals.COLL);
+    }
+
+    @Test
+    public void processProcessEventWithInvalidEventTest() {
+        final var event = new Event(EventType.CREATED) {
+            @Override
+            public EventType getType() {
+                return super.getType();
+            }
+        };
+        Assertions.assertThrows(IllegalStateException.class, () -> EventProcessorHelper.processEvent(event));
+    }
+
+    @Test
+    public void processBulkEntityEventTest() throws IOException, InterruptedException {
+        TestUtils.createTestDatabaseAndCollection();
+        final var testObj = new JsonObject();
+        testObj.add("myField", "myValue");
+        List<DbEntry> insertedEntries = new ArrayList<>(){{ add(DbEntry.fromJsonObject(TestGlobals.DB, TestGlobals.COLL, testObj)); }};
+        final var bulkEntityEvent = new BulkEntityEvent(TestGlobals.DB, TestGlobals.COLL, insertedEntries, new ArrayList<>());
+        EventProcessorHelper.processEvent(bulkEntityEvent);
+        final var adminCollEntry = AdminOperationHelper.getCollectionEntry(TestGlobals.DB, TestGlobals.COLL);
+        Assertions.assertEquals(adminCollEntry.getEntryCount(), insertedEntries.size(), "Entry count doesn't match");
+    }
+
+    @Test
+    public void processCreateEntityEventTest() throws IOException, InterruptedException {
+        final var entityEvent = new EntityEvent(EventType.CREATED, TestGlobals.DB, TestGlobals.COLL, new DbEntry());
+        AdminOperationHelper.saveDatabaseEntry(new AdminDbEntry(TestGlobals.DB));
+        AdminOperationHelper.saveCollectionEntry(new AdminCollEntry(TestGlobals.DB, TestGlobals.COLL));
+        EventProcessorHelper.processEvent(entityEvent);
+        final var collEntry = AdminOperationHelper.getCollectionEntry(TestGlobals.DB, TestGlobals.COLL);
+        Assertions.assertNotNull(collEntry);
+        Assertions.assertEquals(collEntry.getEntryCount(), 1, "Entry count should be 1");
+    }
+
+    @Test
+    public void processIndexEventDeletionTest() throws IOException, InterruptedException {
+        TestUtils.createTestDatabaseAndCollection();
+        final var createIndexEvent = new IndexEvent(EventType.CREATED, TestGlobals.DB, TestGlobals.COLL, "myField");
+        EventProcessorHelper.processEvent(createIndexEvent);
+        var collEntry = AdminOperationHelper.getCollectionEntry(TestGlobals.DB, TestGlobals.COLL);
+        Assertions.assertNotNull(collEntry);
+        Assertions.assertEquals(collEntry.getIndexes().size(), 1, "Index count should be 1");
+        final var deleteIndexEvent = new IndexEvent(EventType.DELETED, TestGlobals.DB, TestGlobals.COLL, "myField");
+        EventProcessorHelper.processEvent(deleteIndexEvent);
+        collEntry = AdminOperationHelper.getCollectionEntry(TestGlobals.DB, TestGlobals.COLL);
+        Assertions.assertNotNull(collEntry);
+        Assertions.assertEquals(collEntry.getIndexes().size(), 0, "Index count should be 0");
+    }
+}
---
+++ src/test/java/org/techhouse/test/TestUtils.java
@@ -0,0 +1,48 @@
+package org.techhouse.test;
+
+import org.techhouse.cache.Cache;
+import org.techhouse.config.Configuration;
+import org.techhouse.data.admin.AdminCollEntry;
+import org.techhouse.data.admin.AdminDbEntry;
+import org.techhouse.fs.FileSystem;
+import org.techhouse.ioc.IocContainer;
+import org.techhouse.ops.AdminOperationHelper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestUtils {
+    public static void standardInitialSetup() throws NoSuchFieldException, IllegalAccessException, IOException {
+        final var config = Configuration.getInstance();
+        final var field = Configuration.class.getDeclaredField("filePath");
+        field.setAccessible(true);
+        field.set(config, "test");
+        final var fs = IocContainer.get(FileSystem.class);
+        fs.createBaseDbPath();
+        fs.createAdminDatabase();
+        final var cache = IocContainer.get(Cache.class);
+        cache.loadAdminData();
+    }
+
+    public static void standardTearDown() throws IOException, InterruptedException {
+        AdminOperationHelper.deleteDatabaseEntry("myDb");
+        deleteDir(new File("test"));
+    }
+
+    private static void deleteDir(File file) throws IOException {
+        File[] contents = file.listFiles();
+        if (contents != null) {
+            for (File f : contents) {
+                deleteDir(f);
+            }
+        }
+        if (!file.delete()) {
+            throw new IOException("File " + file.getName() + " was not deleted");
+        }
+    }
+
+    public static void createTestDatabaseAndCollection() throws IOException, InterruptedException {
+        AdminOperationHelper.saveDatabaseEntry(new AdminDbEntry("myDb"));
+        AdminOperationHelper.saveCollectionEntry(new AdminCollEntry("myDb", "myCollection"));
+    }
+}
